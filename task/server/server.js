89235bed149e4d1c58576aeda96b55fa1a14951a
Parul <bhadoriaparul4@gmail.com>
2026-05-27 18:11:22 +0530

    Fixed Nodemailer Render connection issue

--- task/server/server.js
+++ task/server/server.js
@@ -11,10 +11,16 @@ app.use(cors({
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+
+  service:"gmail",
+
+  port:587,
+
+  secure:false,
+
+  auth:{
+    user:process.env.EMAIL_USER,
+    pass:process.env.EMAIL_PASS
   }
 });
 transporter.verify(function(error, success){
